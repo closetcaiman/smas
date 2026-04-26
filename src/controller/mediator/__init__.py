3dd2a191284160422ccda435c01be5676a881156
Galakitkkon <malekmarek56@gmail.com>
2026-04-26 14:10:24 +0200

    fix: docstring

--- src/controller/mediator/__init__.py
+++ src/controller/mediator/__init__.py
@@ -1,8 +1,8 @@
 """
 Module: controller.mediator.
 
-This module contains the SimulationMediator class, which serves as a central hub for communication between different components of the simulation. The mediator facilitates interactions between the SimulationDataBank, which stores historical data, and the SimulationLogger, which logs events for analysis.
-The SimulationMediator allows for decoupled interactions, ensuring that changes in one component do not directly affect others. It handles events such as births and deaths of agents, dispatching relevant information to the
+This module contains the SimulationMediator class, which serves as a central hub for communication between different components of the simulation.
+The mediator facilitates interactions between the SimulationDataBank, which stores historical data, and the SimulationLogger, which logs events for analysis.
 """
 
 from .simulation_data_bank import SimulationDataBank
